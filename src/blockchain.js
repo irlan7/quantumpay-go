fb4884e7162e9383c98a990540cdee7479735cb9
Irlan <irlan@example.com>
2025-12-03 09:28:48 +0700

    Initial QuantumPay working snapshot

---
+++ src/blockchain.js
@@ -0,0 +1,47 @@
+const Block = require('./block');
+const QHC = require('./consensus/qhc'); // pastikan file ini ada dan mengekspor class QHC
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 3;
+    this.consensus = new QHC();
+  }
+
+  createGenesisBlock() {
+    return new Block(0, Date.now(), "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  // nodes = array of node identifiers (mis. ['NodeA','NodeB'])
+  addBlock(newBlock, nodes = []) {
+    let leader = "Unknown";
+    if (nodes.length > 0) {
+      leader = this.consensus.selectLeader(nodes);
+      // gunakan backticks supaya ${leader} dievaluasi
+      console.log(`Consensus selected leader: ${leader}`);
+    }
+
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+
+    console.log(`Block mined: ${newBlock.hash}`);
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const current = this.chain[i];
+      const previous = this.chain[i - 1];
+
+      if (current.hash !== current.calculateHash()) return false;
+      if (current.previousHash !== previous.hash) return false;
+    }
+    return true;
+  }
+}
+
+module.exports = Blockchain;
